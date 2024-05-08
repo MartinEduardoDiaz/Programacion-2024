57c6317998c0432dad29acaab3faf23fe6c1c5d6
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-05-08 15:55:33 -0400

    Cambios

--- 02_Tipos_de_Datos.py
+++ 02_Tipos_de_Datos.py
@@ -1,3 +1,15 @@
+# Definición de Variable
+
 saludo = "Hola Mundo!"
 
 print(saludo[0])
+
+
+# Concatenación
+
+print(saludo+" "+"Hola hola")
+
+
+# a
+
+print((saludo+" ")*4)
---
+++ print(len(institucion)).py
@@ -0,0 +1,8 @@
+institucion = "Universidad de los Lagos"
+
+carrera = "Ingenieria Civil en Informatica"
+
+
+print(len(institucion))
+
+print(institucion.split)
