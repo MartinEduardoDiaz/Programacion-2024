93f8514db8b30fe57105f3462e16f5f65951d787
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-05-08 16:46:53 -0400

    Reto 1

--- Retos/Reto1.py
+++ Retos/Reto1.py
@@ -1,10 +1,10 @@
 
 nombre = input("¿Cual es tu nombre? ")
 
-edad = input("¿Cual es tu edad? ")
+edad = int(input("¿Cual es tu edad? "))
 
 total_edad = edad + 20
 
 
 
-print("Hola mi nombre es", {nombre}, "tengo", {edad}, "años y en 20 años tendré")
+print("Hola mi nombre es", nombre, "tengo", edad, "años y en 20 años tendré", total_edad)
