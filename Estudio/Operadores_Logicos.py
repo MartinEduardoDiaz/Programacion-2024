451e2fb99f6c75284d96f551eabfdbb4c70282a6
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-06-14 09:21:44 -0400

    Operadores Logicos

---
+++ Estudio/Operadores_Logicos.py
@@ -0,0 +1,19 @@
+
+print()
+
+
+# Arrancar un Auto
+
+encendido = input("¿Enciendes el Auto? ")
+bencina = True
+edad = 19
+
+print()
+
+# Operador AND
+if bencina and encendido:
+    print("El auto puede arrancar")
+else:
+    print("El auto no puede arrancar")
+
+print()
