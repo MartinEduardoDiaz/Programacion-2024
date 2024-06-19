f0b102415545638faa67c20e2bac4f8e7ca75d1b
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-06-19 16:18:08 -0400

    ajustes

---
+++ Estudio/Funciones Personalizadas.py
@@ -0,0 +1,23 @@
+
+# Crear una Función
+
+def saludo(nombre):
+    print("Hola mi nombre es: ", nombre)
+
+
+
+
+# Función con input
+
+def funcion(x):
+    return x*10
+
+y = funcion(int(input("Ingrese un numero: ")))
+
+print(y)
+
+
+
+# a
+
+saludo("Martin")
--- Retos/Reto4.py
+++ Retos/Reto4.py
@@ -2,8 +2,13 @@ print()
 
 numero = 10
 
+
 while numero <= 50:
     print(numero)
     numero+=2
 
 print()
+
+aaa = sum(range(10, 50))
+
+print(aaa)
