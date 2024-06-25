3b1b6781fff42154535b1cbb2ca8e4dc10470180
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-06-25 11:57:13 -0400

    Ejercicios

---
+++ Estudio/Ejercicios/150 Caracteres.py
@@ -0,0 +1,12 @@
+print()
+
+# Ingresar 150 caracteres e indicar cuantas veces se repite el carácter 'a'
+
+caracteres = input("Ingrese 150 caracteres: ")
+
+while len(caracteres) <=150:
+    caracteres = input("Ingrese 150 caracteres: ")
+    
+
+
+print()
---
+++ Estudio/Ejercicios/Adivinar un Numero.py
@@ -0,0 +1,18 @@
+print()
+
+numero = int(input("Ingrese un número: "))
+
+
+while True:
+    numero_pregunta = int(input("Ingrese un número inferior al número: "))
+    if numero_pregunta > 0:
+        numero > 0
+        numero_mayor = int(input("El número es positivo, Ingrese un número mayor a el número: "))
+        numero < numero_mayor
+        
+
+
+
+
+
+print()
