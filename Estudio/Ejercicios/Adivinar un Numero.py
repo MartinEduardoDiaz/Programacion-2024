07cf8e713ceb56d1fa6cad6458d9329648a5cb9d
MartinEduardoDiaz <168034374+MartinEduardoDiaz@users.noreply.github.com>
2024-06-25 12:09:54 -0400

    Update

--- Estudio/Ejercicios/Adivinar un Numero.py
+++ Estudio/Ejercicios/Adivinar un Numero.py
@@ -4,13 +4,13 @@ numero = int(input("Ingrese un número: "))
 
 
 while True:
-    numero_pregunta = int(input("Ingrese un número inferior al número: "))
-    if numero_pregunta > 0:
+    numero_inferior = int(input("Ingrese un número inferior al número: "))
+    if numero_inferior > 0:
         numero > 0
         numero_mayor = int(input("El número es positivo, Ingrese un número mayor a el número: "))
         numero < numero_mayor
-        
-
+        print("Tu número es ", numero)
+        break
 
 
 
